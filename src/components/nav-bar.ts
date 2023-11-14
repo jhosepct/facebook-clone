cbef0f507cd9a4617c96b73e2ecb1b8e8d64279a
Kevin <kevinjhosepct@gmail.com>
2023-11-14 11:30:00 -0500

    Add nav-bar component

---
+++ src/components/nav-bar.ts
@@ -0,0 +1,93 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js'
+import { Header, HeaderType } from '../mocks/navigation';
+
+
+@customElement('nav-bar')
+export class NavBar extends LitElement {
+
+    @property({ type: Array }) navigation: HeaderType['navigation'];
+    @property({ type: Array }) info: HeaderType['info'];
+
+    constructor() {
+        super();
+        this.navigation = Header.navigation;
+        this.info = Header.info;
+    }
+
+    render() {
+        return html`
+            <div class="header-container">
+                <div class="search-bar">
+                    <avatar-circle width="55px" height="55px" src="https://www.quinneysbush.co.nz/wp-content/uploads/2021/04/cropped-Facebook-logo.png"></avatar-circle>
+                    <custom-input type="search" placeholder="Search Facebook" class="search-input"></custom-input>
+                </div>
+                <nav class="navigation">
+                    <ul class="nav-list">
+                        ${this.navigation.map(item => html`
+                            <li><a href="${item.url}">
+                                <button-nav name="${item.name}" type="rect" icon="${item.icon}"></button-nav>
+                            </a></li>
+                        `)}
+                    </ul>
+                </nav>
+                <div class="user-info">
+                    ${this.info.map(item => html`
+                        <button-nav count="${item.count ? item.count : 0}" icon="${item.icon}"></button-nav>
+                    `)}
+                    <avatar-circle src="https://i.pravatar.cc/150?img=3"></avatar-circle>
+                </div>
+            </div>
+        `;
+    }
+
+    static styles = [
+        css`
+           .header-container {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                padding: 8px 16px;
+                background-color: #242526;
+            }
+
+            /* Estilos de la barra de búsqueda */
+            .search-bar {
+                display: flex;
+                gap:8px;
+                align-items: center;
+            }
+
+            /* Estilos de la barra de navegación */
+            .navigation {
+                display: flex;
+                align-items: center;
+            }
+
+            .nav-list {
+                display: flex;
+                list-style: none;
+                margin: 0;
+                padding: 0;
+            }
+
+            .nav-list li {
+                margin-right: 16px;
+            }
+
+            .nav-list a {
+                text-decoration: none;
+                color: #333;
+                font-weight: bold;
+            }
+
+            /* Estilos de la información del usuario */
+            .user-info {
+                display: flex;
+                align-items: center;
+                margin-left: 16px;
+                gap:8px;
+            }
+        `
+    ];
+}
