2f4b2e31b771b73db3d6fdceccec6b9b3cbe5a55
Kevin <kevinjhosepct@gmail.com>
2023-11-14 19:21:27 -0500

    Modified property of active

--- src/components/avatar-circle.ts
+++ src/components/avatar-circle.ts
@@ -6,9 +6,13 @@ export class AvatarCircle extends LitElement {
     @property({ type: String }) src = '';
     @property({ type: String }) width = '56px';
     @property({ type: String }) height = '56px';
+    @property({ type: Boolean }) active = false;
 
     static styles = [
         css`
+            .avatar{
+                position: relative;
+            }
             .avatar, .avatar img{
                 border-radius: 50%;
             }
@@ -18,12 +22,28 @@ export class AvatarCircle extends LitElement {
                 height: 100%;
                 object-fit: cover;
             }
+
+            .avatar .notification{
+                position: absolute;
+                min-width: 8px;
+                min-height: 8px;
+                right:  0px;
+                bottom: 0px;
+                background-color: #31a24c;
+                border: 3px solid #1a1b1c;
+                border-radius: 50%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-size: 14px;
+            }
         `
     ];
 
     render() {
         return html`
             <div class="avatar" style="width: ${this.width}; height: ${this.height}">
+                ${this.active ? html`<div class="notification"></div>` : ''}
                 <img src="${this.src}" alt="Avatar">
             </div>
         `;
