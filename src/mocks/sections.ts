24e1003f443c32026b06f25724ea512061a7b42d
Kevin <kevinjhosepct@gmail.com>
2023-11-14 19:20:13 -0500

    Add section mocks

---
+++ src/mocks/sections.ts
@@ -0,0 +1,44 @@
+interface Section {
+    style: string;
+    name: string;
+    href: string;
+}
+
+export const sections: Section[] = [
+    {
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yz/r/4GR4KRf3hN2.png?_nc_eui2=AeFj4YVKB02E-NtEA3yCIogViXYVTRy2fzKJdhVNHLZ_MkCLckbcnnoml_GB2XBLstuRjwyMYJ7fpOdCvlfTQRjx');background-position:0 -296px;background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Friends',
+        href: '#',
+    },
+    {
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yz/r/4GR4KRf3hN2.png?_nc_eui2=AeFj4YVKB02E-NtEA3yCIogViXYVTRy2fzKJdhVNHLZ_MkCLckbcnnoml_GB2XBLstuRjwyMYJ7fpOdCvlfTQRjx');background-position:0 -444px;background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Memories',
+        href: '#',
+    },
+    {
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yz/r/4GR4KRf3hN2.png?_nc_eui2=AeFj4YVKB02E-NtEA3yCIogViXYVTRy2fzKJdhVNHLZ_MkCLckbcnnoml_GB2XBLstuRjwyMYJ7fpOdCvlfTQRjx');background-position:0 -185px;background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Saved',
+        href: '#',
+    },
+    {
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yz/r/4GR4KRf3hN2.png?_nc_eui2=AeFj4YVKB02E-NtEA3yCIogViXYVTRy2fzKJdhVNHLZ_MkCLckbcnnoml_GB2XBLstuRjwyMYJ7fpOdCvlfTQRjx');background-position:0 -37px;background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Groups',
+        href: '#',
+    },
+    {
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yz/r/4GR4KRf3hN2.png?_nc_eui2=AeFj4YVKB02E-NtEA3yCIogViXYVTRy2fzKJdhVNHLZ_MkCLckbcnnoml_GB2XBLstuRjwyMYJ7fpOdCvlfTQRjx');background-position:0 -518px;background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Video',
+        href: '#',
+    },
+    {
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yz/r/4GR4KRf3hN2.png?_nc_eui2=AeFj4YVKB02E-NtEA3yCIogViXYVTRy2fzKJdhVNHLZ_MkCLckbcnnoml_GB2XBLstuRjwyMYJ7fpOdCvlfTQRjx');background-position:0 -407px;background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Marketplace',
+        href: '#',
+    },
+    {
+
+        style: `background-image:url('https://static.xx.fbcdn.net/rsrc.php/v3/yb/r/eECk3ceTaHJ.png?_nc_eui2=AeHMuz3bLMe8jeWaiHh72Cebi9AM8exRZGeL0Azx7FFkZ9wS7Ycvrr4jZAy9hrGxM0JiXunv9TMIw2LvXdnlHr-4');background-size:auto;width:36px;height:36px;background-repeat:no-repeat;display:inline-block`,
+        name: 'Feeds',
+        href: '#',
+    }
+]
